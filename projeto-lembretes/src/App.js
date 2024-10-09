344e88cc33f103b0e0383118f4c238e8e7c75345
João Gabriel <jgabriel.ferreira20@gmail.com>
2024-10-09 17:35:26 -0300

    docs(p1-pdmt): inclui nomes e ras

---
+++ projeto-lembretes/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12   
+ text-center">
+          <h1 className='display-1'>Hello, lembretes!</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
